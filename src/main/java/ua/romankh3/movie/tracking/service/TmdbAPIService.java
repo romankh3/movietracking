b749d91b19ae0d0b2ad25b9b00290af58cef7b1a
Roman Beskrovniy <roman.beskrovniy@l1inc.com>
2018-06-12 22:36:03 +0300

    updated TMDB service for an parameters.

--- src/main/java/ua/romankh3/movie/tracking/service/TmdbAPIService.java
+++ src/main/java/ua/romankh3/movie/tracking/service/TmdbAPIService.java
@@ -1,8 +1,22 @@
 package ua.romankh3.movie.tracking.service;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface TmdbAPIService {
 
     String retrieveMovies(String path) throws IOException;
+
+    String retrieveMovies(String path, String primaryReleaseYear);
+
+    String retrieveMovies(String path, String primaryReleaseYear, String primaryReleaseMonth);
+
+    String retrieveMovies(String path, List<Integer> favoriteActorIds);
+
+    String retrieveMovies(String path, String primaryReleaseYear, List<Integer> favoriteActorIds);
+
+    String retrieveMovies(String path,
+                          String primaryReleaseYear,
+                          String primaryReleaseMonth,
+                          List<Integer> favoriteActorIds);
 }
--- src/main/java/ua/romankh3/movie/tracking/service/impl/TmdbAPIServiceImpl.java
+++ src/main/java/ua/romankh3/movie/tracking/service/impl/TmdbAPIServiceImpl.java
@@ -12,6 +12,7 @@ import ua.romankh3.movie.tracking.service.TmdbAPIService;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 
 @Service
@@ -26,10 +27,51 @@ public class TmdbAPIServiceImpl implements TmdbAPIService {
     @Value("${tmdb.api.base.url}")
     private String tmdbApiBaseUrl;
 
+    public static final String API_KEY = "api_key";
+    public static final String LANGUAGE = "language";
+    public static final String PRIMARY_RELEASE_YEAR = "primary_release_year";
+    public static final String PRIMARY_RELEASE_MONTH = "???";
+    public static final String FAVORITE_ACTORS = "people";
+
     @Override
     public String retrieveMovies(String path) throws IOException {
+        return callToTMDB(path, null, null, null);
+    }
+
+    @Override
+    public String retrieveMovies(String path, String primaryReleaseYear) {
+        return callToTMDB(path, primaryReleaseYear, null, null);
+    }
+
+    @Override
+    public String retrieveMovies(String path, String primaryReleaseYear, String primaryReleaseMonth) {
+        return callToTMDB(path, primaryReleaseYear, primaryReleaseMonth, null);
+    }
+
+    @Override
+    public String retrieveMovies(String path, List<Integer> favoriteActorIds) {
+        return callToTMDB(path, null, null, favoriteActorIds);
+    }
+
+    @Override
+    public String retrieveMovies(String path, String primaryReleaseYear, List<Integer> favoriteActorIds) {
+        return callToTMDB(path, primaryReleaseYear, null, favoriteActorIds);
+    }
+
+    @Override
+    public String retrieveMovies(String path,
+                                 String primaryReleaseYear,
+                                 String primaryReleaseMonth,
+                                 List<Integer> favoriteActorIds) {
+        return callToTMDB(path, primaryReleaseYear, primaryReleaseMonth, favoriteActorIds);
+    }
+
+    private String callToTMDB(String path,
+                              String primaryReleaseYear,
+                              String primaryReleaseMonth,
+                              List<Integer> favoriteActorIds) {
         try {
-            String url = createTmdbUrl(path);
+            String url = createTmdbUrl(path, primaryReleaseYear, primaryReleaseMonth, favoriteActorIds);
             HttpResponse<JsonNode> jsonResponse = Unirest.get(url).asJson();
 
             if(jsonResponse.getStatus() != HttpStatus.SC_OK) {
@@ -43,11 +85,17 @@ public class TmdbAPIServiceImpl implements TmdbAPIService {
         return null;
     }
 
-    private String createTmdbUrl(String s) throws URISyntaxException {
-        URIBuilder uriBuilder = new URIBuilder(tmdbApiBaseUrl + s);
-        uriBuilder.addParameter("language", tmdbLanguage);
-        uriBuilder.addParameter("api_key", tmdbApiKey);
-        uriBuilder.addParameter("primary_release_year", "2010");
+    private String createTmdbUrl(String path,
+                                 String primaryReleaseYear,
+                                 String primaryReleaseMonth,
+                                 List<Integer> favoriteActors) throws URISyntaxException {
+        // todo implement the logic favorite actors and primary release date.
+        URIBuilder uriBuilder = new URIBuilder(tmdbApiBaseUrl + path);
+        uriBuilder.addParameter(LANGUAGE, tmdbLanguage);
+        uriBuilder.addParameter(API_KEY, tmdbApiKey);
+        // todo implement joining ids in string.
+        uriBuilder.addParameter(FAVORITE_ACTORS, null);
+        uriBuilder.addParameter(PRIMARY_RELEASE_YEAR, primaryReleaseYear);
         return uriBuilder.build().toString();
     }
 }
